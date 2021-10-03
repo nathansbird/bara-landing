e4572214cb53bc919014f7bf338f23a69478b2a7
nathansbird <chosenbird1@gmail.com>
2021-10-03 00:45:46 -0500

    Compatibility

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,6 @@ const Home = () => {
       updateSizing();
     }
   }, [])
-
-  console.log(sizing.width, sizing.height, sizing.width > sizing.height ? "hidden" : "unset")
   
   const sizeToUse = sizing.width * 2.10 < sizing.height ? "HEIGHT" : "WIDTH";
 
@@ -28,11 +26,13 @@ const Home = () => {
         autoPlay={true}
         preload="true"
         muted
+        playsInline
         loop={true}
         style={{
           display: sizing.width > sizing.height ? "unset" : "none"
         }}>
-        <source src="/assets/bara-horizontal.webm"></source>
+        <source src="/assets/horizontal.webm" type="video/webm"></source>
+        <source src="/assets/horizontal.mp4" type="video/mp4"></source>
       </video> 
       <video id="background-v"
         width={sizeToUse == "WIDTH" ? sizing.width : undefined}
@@ -40,11 +40,13 @@ const Home = () => {
         autoPlay={true}
         preload="true"
         muted
+        playsInline
         loop={true}
         style={{
           display: sizing.width > sizing.height ? "none" : "unset"
         }}>
-        <source src="/assets/bara-vertical.webm"></source>
+        <source src="/assets/vertical.webm" type="video/webm"></source>
+        <source src="/assets/vertical.mp4" type="video/mp4"></source>
       </video>
       <div id="center">
         <img className="primary-logo" src="/assets/Layer_2.svg"/>
